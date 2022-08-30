469223e6826d56b152d65fb0443029e157d14d43
enchance <enchance@gmail.com>
2022-08-30 11:50:02 +0800

    update movie contract

---
+++ 2022-08-30/movies.sol
@@ -0,0 +1,64 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Gas fee: 559294
+
+
+contract Shows {
+    address owner;
+    string anime = "One Punch Man";
+    string movie;
+    uint age;
+    address doctor;
+
+    error InvalidAge();   // Only works with revert not require
+
+    constructor(string memory _movie, uint _age, address _doctor) {
+        owner = msg.sender;
+        movie = _movie;
+        age = _age;
+        doctor = _doctor;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    modifier onlyAge() {
+        // require(age == 39, "You are not the right age");
+        if(age != 39) {
+            revert InvalidAge();
+        }
+        _;
+    }
+
+    modifier onlyDoctor() {
+        require(msg.sender == doctor, "You are not my doctor");
+        _;
+    }
+
+    function getAnime() public view onlyOwner returns (string memory) {
+        return anime;
+    }
+
+    function setAnime(string memory _anime) public onlyOwner onlyAge {
+        // require(age == 39, "You are not the right age");
+        // if (msg.sender != owner) {
+        //     revert("You are not the owner");
+        // }
+        anime = _anime;
+    }
+
+    function getMovie() public view onlyOwner returns (string memory) {
+        return movie;
+    }
+
+    function getMedicalData() public view onlyDoctor returns (uint) {
+        return 789;
+    }
+
+    function setDoctor(address newdoctor) public onlyOwner {
+        doctor = newdoctor;
+    }
+}
---
+++ 2022-08-30/notes.md
@@ -0,0 +1,10 @@
+
+Notes
+===============
+
+
+
+If a condition fails
+
+1. Stop everything and exit the contract
+2. Return any payments other than the gas fee
