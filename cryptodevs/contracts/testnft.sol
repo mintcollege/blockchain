d2ca26398f443f24d29bf059c359452ee6d0ffd5
enchance <enchance@gmail.com>
2022-11-22 14:35:27 +0800

    nft intro

---
+++ cryptodevs/contracts/testnft.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract TestToken is ERC721, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Test Token", "TKN") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://buycatnfts.com";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _updatesuri() private {}
+
+    function _callothercontract() external {}
+}
